bd6394c66bdd17fd8ed4a6cbe36209c84af5719c
anvh2 <anvo.itus209@gmail.com>
2019-12-21 11:35:18 +0700

    remove ts-build

--- ts-build/src/plugins/sse/sse.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var SSE = /** @class */ (function () {
-    function SSE() {
-    }
-    return SSE;
-}());
-exports.SSE = SSE;
