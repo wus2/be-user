4fe3d4e5010c8512990a89de7579f72c0053b367
anvh2 <anvo.itus209@gmail.com>
2019-12-26 20:32:08 +0700

    fix socket

--- src/plugins/socket/socket.ts
+++ src/plugins/socket/socket.ts
@@ -42,7 +42,7 @@ export class SocketServer implements ISocketServer {
       handlePreflightRequest: (req, res) => {
         const headers = {
           "Access-Control-Allow-Headers": "Content-Type, Authorization",
-          "Access-Control-Allow-Origin": req.headers.origin, //or the specific origin you want to give access to,
+          "Access-Control-Allow-Origin": req.headers.origin,
           "Access-Control-Allow-Credentials": true
         };
         res.writeHead(200, headers);
