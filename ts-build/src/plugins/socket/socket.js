bd1ae0264011a38530bc6acebdfc7ee01b67ff92
anvh2 <anvo.itus209@gmail.com>
2019-12-26 11:13:14 +0700

    compile to js

--- ts-build/src/plugins/socket/socket.js
+++ ts-build/src/plugins/socket/socket.js
@@ -51,7 +51,7 @@ var Event;
 var SocketServer = /** @class */ (function () {
     function SocketServer(server) {
         this.io = socket_io_1.default(server, {
-            origins: "http://112.197.2.178:8004:*"
+            origins: "*"
         });
         this.clients = new Map();
         this.messageDB = new message_1.MessageDB();
