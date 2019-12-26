42a4c8fc58d8a4c0a8d4afe94b944533c22f1ca0
anvh2 <anvo.itus209@gmail.com>
2019-12-26 13:17:20 +0700

    compile to js

--- ts-build/src/plugins/socket/socket.js
+++ ts-build/src/plugins/socket/socket.js
@@ -50,9 +50,7 @@ var Event;
 })(Event = exports.Event || (exports.Event = {}));
 var SocketServer = /** @class */ (function () {
     function SocketServer(server) {
-        this.io = socket_io_1.default(server, {
-            origins: "*"
-        });
+        this.io = socket_io_1.default(server, { origins: "*:*" });
         this.clients = new Map();
         this.messageDB = new message_1.MessageDB();
         this.secretKey = config_1.default.get("key_jwt");
